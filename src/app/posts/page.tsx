328ae015643ac60d8536e4bf44ff496ee2654fb0
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-03-09 16:25:39 +0000

    Create posts route

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,26 @@
+const posts: { title: string; date: string; description: string; }[] = [
+  {
+    title: "first post",
+    date: "2025-03-09",
+    description: "first post wow",
+  },
+  {
+    title: "second post",
+    date: "2025-02-10",
+    description: "second post wow",
+  },
+];
+
+export default function Posts() {
+  return (
+    <div className="container mx-auto p-4">
+      {posts.map((post) => (
+        <div key={post.title} className="bg-white shadow-md rounded-lg p-3 mb-4">
+          <h1 className="text-2xl font-bold mb-2">{post.title}</h1>
+          <p className="text-gray-700">{post.description}</p>
+          <p>{post.date}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
