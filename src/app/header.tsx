2b7622e5b233f4bc527bcbd96ec7499635646477
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-03-10 10:21:05 +0000

    Make header and content spacing universal

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex justify-center text-2xl h-16 bg-white p-4 font-bold w-full fixed top-0 border-red-100">
+    <header className="flex justify-center text-2xl bg-white p-4 font-bold w-full fixed top-0 border-red-100">
       <h1>Hello</h1>
     </header>
   )
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -13,7 +13,7 @@ const posts: { title: string; date: string; description: string; }[] = [
 
 export default function Posts() {
   return (
-    <div className="container mx-auto p-4 mt-16">
+    <main className="container mx-auto p-4">
       {posts.map((post) => (
         <div key={post.title} className="bg-white shadow-md p-2 mb-2">
           <h1 className="text-2xl font-bold mb-2">{post.title}</h1>
@@ -21,6 +21,6 @@ export default function Posts() {
           <p>{post.date}</p>
         </div>
       ))}
-    </div>
+    </main>
   );
 }
