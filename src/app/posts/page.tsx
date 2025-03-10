339178a2ce452be6fd6d7f252a19cd561f029644
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-03-10 16:34:19 +0000

    Remove unnecessary class

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,7 +4,7 @@ let posts = getPosts()
 
 export default function Posts() {
   return (
-    <div className="posts-container space-y">
+    <div className="posts-container">
       {posts.map((post) => (
         <div key={post.title} className="border-b-2 p-2">
           <h1 className="text-2xl font-bold mb-1">{post.title}</h1>
