3cfbd4340890635d140ffc87a4653c9c7c644505
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-03-10 15:49:32 +0000

    Vertically align the header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className="border-b-4 border-red-500">
-      <h1 className="text-2xl font-bold ml-2 h-10">Paper Fitness</h1>
+    <header className="border-b-4 border-red-500 h-12 flex items-center">
+      <h1 className="text-2xl font-bold ml-2">Paper Fitness</h1>
     </header>
   );
 }
