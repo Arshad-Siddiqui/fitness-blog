150994e35378dae962a2783b05b5f8f4466b6e6f
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-03-10 16:06:48 +0000

    Have sidebar go full screen height

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -13,9 +13,9 @@ const posts: { title: string; date: string; description: string; }[] = [
 
 export default function Posts() {
   return (
-    <div className="container mx-auto">
+    <div className="posts-container">
       {posts.map((post) => (
-        <div key={post.title} className="border-4 p-2 mb-2">
+        <div key={post.title} className="border-2 p-2">
           <h1 className="text-2xl font-bold mb-1">{post.title}</h1>
           <p className="text-gray-700">{post.description}</p>
           <p>{post.date}</p>
--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -1,7 +1,8 @@
 export default function Sidebar() {
   return (
-    <div className="sidebar border-r-4 border-blue-500">
-      <h2>Sidebar</h2>
+    <div className="sidebar border-r-4 border-blue-500 w-14 h-screen">
+      <div className="top-box h-12 border-b-4 border-red-500" />
+      <div className="bottom-box" />
     </div>
   );
 }
