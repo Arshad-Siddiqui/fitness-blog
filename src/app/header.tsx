9af2e6e191e27ce0f1f03f7f356b8e7357ce4e6e
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-03-10 10:29:56 +0000

    Give header a button and space the items

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,8 @@
 export default function Header() {
   return (
-    <header className="flex justify-center text-2xl bg-white p-4 font-bold w-full fixed top-0 border-red-100">
-      <h1>Hello</h1>
+    <header className="flex justify-between items-center bg-white p-4 w-full fixed top-0 border-b-4 border-red-500">
+      <button className="text-blue-500">Menu</button>
+      <h1 className="absolute left-1/2 transform -translate-x-1/2 font-bold text-2xl">Paper Fitness</h1>
     </header>
-  )
+  );
 }
