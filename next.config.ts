96936334d3a1c4e522bbe29e7a699bded0d77c40
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-03-10 10:21:25 +0000

    Reshuffle the MDX stuff

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/
-});
-
+import createMDX from '@next/mdx'
+ 
 /** @type {import('next').NextConfig} */
-module.exports = withMDX({
-  experimental: { appDir: true },
-  pageExtensions: ["ts", "tsx", "mdx"]
-});
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
