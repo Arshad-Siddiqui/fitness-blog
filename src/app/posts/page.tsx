3427c313aa00e500853cbbb8e9711f4ecdd4b7f4
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-13 11:28:02 +0000

    Add key to each post in map

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,4 @@
-import { getPosts } from "../getPosts";
+import getPosts from "../functions/getPosts";
 import Link from "next/link";
 
 let posts = getPosts()
@@ -7,10 +7,10 @@ export default function Posts() {
   return (
     <div className="posts-container">
       {posts.map((post) => (
-        <Link href="">
-          <div key={post.title} className="border-b-2 p-2">
+        <Link key={post.title} href="">
+          <div className="border-b-2 p-2">
             <h1 className="text-2xl font-bold mb-1">{post.title}</h1>
-            <p>{post.date}</p>
+            <p className="text-grey-600">{post.date}</p>
           </div>
         </Link>
       ))}
