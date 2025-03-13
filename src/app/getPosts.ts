db6d87b234369004cfa8a1ab1e3da7d3785aa848
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-13 11:09:25 +0000

    Add comments to getPost and log when a file is missing metadata

--- src/app/getPosts.ts
+++ src/app/getPosts.ts
@@ -1,13 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 
+// postsDirectory is the absolute path to /contents
 const postsDirectory = path.join(process.cwd(), '/content');
 
 export function getPosts() {
+  // Gets all file names from that directory
   const filenames = fs.readdirSync(postsDirectory);
 
+  // Returns an array of objects containing metadata of file
   return filenames.map((filename) => {
     const module = require(`../../content/${filename}`); // Dynamically import
+    
+    if (!module.title || !module.date) {
+      console.log(`Missing title or date in file: ${filename}`);
+    }
+
     return {
       filename,
       title: module.title, // Metadata
