ab7154fc1687266c7e1c19d3d52fac3d6aea9a36
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-13 11:59:47 +0000

    Link posts to slug

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -7,7 +7,7 @@ export default function Posts() {
   return (
     <div className="posts-container">
       {posts.map((post) => (
-        <Link key={post.title} href="">
+        <Link key={post.title} href={"posts/" + post.slug}>
           <div className="border-b-2 p-2">
             <h1 className="text-2xl font-bold mb-1">{post.title}</h1>
             <p className="text-grey-600">{post.date}</p>
