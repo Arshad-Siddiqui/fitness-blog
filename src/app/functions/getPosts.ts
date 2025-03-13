b0cc724dca865244fcb1a367bd4de09391d70146
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-13 11:27:45 +0000

    Move function exports to function folder

--- src/app/functions/getPosts.ts
+++ src/app/functions/getPosts.ts
@@ -4,13 +4,13 @@ import path from 'path';
 // postsDirectory is the absolute path to /contents
 const postsDirectory = path.join(process.cwd(), '/content');
 
-export function getPosts() {
+export default function getPosts() {
   // Gets all file names from that directory
   const filenames = fs.readdirSync(postsDirectory);
 
   // Returns an array of objects containing metadata of file
   return filenames.map((filename) => {
-    const module = require(`../../content/${filename}`); // Dynamically import
+    const module = require(`../../../content/${filename}`); // Dynamically import
     
     if (!module.title || !module.date) {
       console.log(`Missing title or date in file: ${filename}`);
