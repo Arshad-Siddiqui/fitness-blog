3bc3526ed759ecef27f77c35216b5cd44c2a2458
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-14 10:13:25 +0000

    Add a home button

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Sidebar() {
   return (
     <div className="sidebar border-r-4 border-blue-500 w-14 h-screen">
       <div className="top-box h-12 border-b-4 border-red-500" />
-      <div className="bottom-box" />
+      <div className="bottom-box">
+        <Link href="/">
+          <Image src="/house.png" alt="house icon" width="200" height="200" />
+        </Link>
+      </div>
     </div>
   );
 }
