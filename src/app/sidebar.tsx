02fe8db4c394ec805d73bdb98fe66e1833b701c5
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-15 13:01:04 +0000

    Make home button link to posts

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
     <div className="sidebar border-r-4 border-blue-500 w-14 h-screen">
       <div className="top-box h-12 border-b-4 border-red-500" />
       <div className="bottom-box">
-        <Link href="/">
+        <Link href="/posts">
           <Image src="/house.png" alt="house icon" width="200" height="200" />
         </Link>
       </div>
