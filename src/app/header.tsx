abdce6459902a4cc4ee18d917a1387871bc5b657
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-15 13:51:18 +0000

    Added some style to each component

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Header() {
   return (
     <Link href="/">
-      <header className="border-b-4 border-red-500 h-12 flex items-center">
-        <h1 className="text-2xl font-bold ml-2">Paper Fitness</h1>
+      <header className="border-b-4 border-red-500 h-12 flex flex-col">
+        <h1 className="text-2xl font-bold ml-2 mt-auto">Paper Fitness</h1>
       </header>
     </Link>
   );
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,18 @@
-import type { MDXComponents } from 'mdx/types'
- 
+import type { MDXComponents } from 'mdx/types';
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-  }
+    h1: (props) => <h1 className="text-2xl font-bold my-4" {...props} />,
+    h2: (props) => <h2 className="text-xl font-semibold my-3" {...props} />,
+    h3: (props) => <h3 className="text-lg font-medium my-2" {...props} />,
+    p: (props) => <p className="text-gray-700 text-base my-2" {...props} />,
+    a: (props) => <a className="text-red-600 hover:underline" {...props} />,
+    ul: (props) => <ul className="list-disc list-inside my-2" {...props} />,
+    ol: (props) => <ol className="list-decimal list-inside my-2" {...props} />,
+    li: (props) => <li className="my-1" {...props} />,
+    blockquote: (props) => <blockquote className="border-l-4 border-gray-500 pl-4 italic my-4" {...props} />,
+    code: (props) => <code className="bg-gray-100 text-red-500 px-1 rounded" {...props} />,
+    pre: (props) => <pre className="bg-gray-900 text-white p-4 rounded my-4 overflow-x-auto" {...props} />,
+  };
 }
