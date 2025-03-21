9fa9aa822b2cc693ccb0ca1db4c8c415073091f5
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-21 16:49:03 +0000

    Swap header colors

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <Link href="/">
-      <header className="border-b-4 border-red-500 h-12 flex flex-col">
-        <h1 className="text-2xl font-bold ml-1 mt-auto">Paper Fitness</h1>
+      <header className="border-b-4 border-blue-500 h-12 flex items-center justify-center bg-gray-200">
+        <span className="header-left-box border-red-500 border-r-4 h-full" />
+        <h1 className="text-2xl font-bold ml-2">Paper Fitness</h1>
       </header>
     </Link>
   );
