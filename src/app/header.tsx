ea88107ef1aacfb7bbd8fe0fb7cb5073eafcd9ef
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-23 17:44:00 +0000

    Clean up header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <Link href="/">
-      <header className="border-b-4 border-blue-500 h-12 flex items-center justify-center bg-gray-200">
-        <span className="header-left-box border-red-500 border-r-4 h-full" />
-        <h1 className="text-2xl font-bold ml-2">Paper Fitness</h1>
+      <header className="border-b-4 border-blue-500 h-12 bg-gray-200 flex">
+        <div className="header-left-box border-red-500 border-r-4 h-full w-14 flex-none" />
+        <div className="text-2xl font-bold ml-2 flex">Paper Fitness</div>
       </header>
     </Link>
   );
