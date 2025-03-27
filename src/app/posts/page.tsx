0f5f1fcacca9621724a55a970a3945441be7d731
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-27 00:05:01 +0000

    Removed the tacky lined paper look

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,24 +2,20 @@ import getPosts from "../functions/getPosts";
 import Link from "next/link";
 
 let posts = getPosts()
-let lineStyle = "border-b-2 h-10 border-gray-400 flex flex-col p-1"
 
 export default function Posts() {
   return (
-    <div className="posts-container">
+    <article className="flex flex-col justify-center">
       {posts.map((post) => (
         <Link key={post.title} href={"posts/" + post.slug}>
-          <div className={lineStyle}>
+          <div>
             <h2 className="font-bold mt-auto text-lg">{post.title}</h2>
           </div>
-          <div className={lineStyle}>
+          <div>
             <p className="text-grey-600 text-sm mt-auto">{post.date}</p>
-          </div>
-          <div className={lineStyle}>
-            <p className="text-grey-600 text-1/2 mt-auto" />
           </div>
         </Link>
       ))}
-    </div>
+    </article>
   );
 }
