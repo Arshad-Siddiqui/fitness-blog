823a2907be1fd453f06a4bfbab2e2a33c86f0f82
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-27 00:10:09 +0000

    Align header in the center

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function Header() {
   return (
     <Link href="/">
-      <header className="border-b-4 border-blue-500 h-12 bg-gray-200 flex">
+      <header className="border-b-4 border-blue-500 h-12 bg-gray-200 flex items-center">
         <div className="header-left-box border-red-500 border-r-4 h-full w-14 flex-none" />
-        <div className="text-2xl font-bold ml-2 flex">Paper Fitness</div>
+        <div className="text-2xl font-bold ml-2">
+          <h1>Paper Fitness</h1>
+        </div>
       </header>
     </Link>
   );
