d9e7104adfd3d0378bfa705eaf49513590dd7367
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-28 02:03:42 +0000

    Add some style to front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
 
 export default function Home() {
+  const routes = [
+    { name: "Posts", bgColor: "bg-blue-400" },
+    { name: "Programming", bgColor: "bg-green-400" },
+    { name: "Fitness", bgColor: "bg-red-400" },
+  ];
+
   return (
-      <main className="flex justify-center home flex flex-col items-center">
-        <li>
-          <Link href="/posts">Posts</Link>
-        </li>
-        <li>
-          <Link href='/programming'>Programming</Link>
-        </li>
-        <li>
-          <Link href="/fitness">Fitness</Link>
-        </li>
-      </main>
+    <main className="flex justify-center flex flex-col items-center">
+        {routes.map((route) => (
+          <div key={route.name} className="max-w-md w-full m-2">
+            <Link
+              href={"/" + route.name.toLowerCase()}
+              className={`${route.bgColor} block text-center p-2`}
+            >
+              {route.name}
+            </Link>
+          </div>
+        ))}
+    </main>
   );
 }
