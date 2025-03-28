96714aea5373a3a248f1dd7f016e71ec8f2e102c
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-28 01:39:51 +0000

    Added a few more routes to the front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-      <main className="flex justify-center home">
-        <Link href="/posts">Posts</Link>
+      <main className="flex justify-center home flex flex-col items-center">
+        <li>
+          <Link href="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link href='/programming'>Programming</Link>
+        </li>
+        <li>
+          <Link href="/fitness">Fitness</Link>
+        </li>
       </main>
   );
 }
