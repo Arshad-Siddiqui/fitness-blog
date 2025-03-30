602e42cb4c52e72b526d912ab7cd0ce2e72f287f
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-30 12:27:41 +0100

    #filterPosts

--- src/app/functions/getPosts.ts
+++ src/app/functions/getPosts.ts
@@ -1,10 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
+type Post = { filename: string, title: string, date: string, category: string }
+
 // postsDirectory is the absolute path to /contents
 const postsDirectory = path.join(process.cwd(), '/content');
 
-export default function getPosts() {
+export default function getPosts(): Post[] {
   // Gets all file names from that directory
   const filenames = fs.readdirSync(postsDirectory);
 
@@ -12,8 +14,8 @@ export default function getPosts() {
   return filenames.map((filename) => {
     const module = require(`../../../content/${filename}`); // Dynamically import
     
-    if (!module.title || !module.date) {
-      console.log(`Missing title or date in file: ${filename}`);
+    if (!module.title || !module.date || !module.contentType) {
+      console.log(`Missing title, date or content type in file: ${filename}`);
     }
 
     return {
@@ -21,6 +23,11 @@ export default function getPosts() {
       slug: filename.replace(".mdx", ""),
       title: module.title, // Metadata
       date: module.date,
+      category: module.category
     };
   });
+}
+
+export function filterPosts(posts:Post[], category: string): Post[] {
+  return posts.filter(post => post.category = category)
 }
