c3f09949b20f1d14eadd9977c32a935dc8ea5731
arshadsiddiqui <ferozarshad123@gmail.com>
2025-03-31 14:29:37 +0100

    Change routing for individual posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,13 @@
 import { getPosts } from "../functions/getPosts";
 import Link from "next/link";
 
-let posts = getPosts()
+const posts = getPosts()
 
 export default function Posts() {
   return (
     <article className="flex flex-col items-center">
       {posts.map((post) => (
-        <Link key={post.title} href={"posts/" + post.slug} className='w-full max-w-md m-1 bg-gray-200 p-2'>
+        <Link key={post.title} href={"posts/post/" + post.slug} className='w-full max-w-md m-1 bg-gray-200 p-2'>
           <div>
             <h2 className="font-bold mt-auto text-lg">{post.title}</h2>
           </div>
