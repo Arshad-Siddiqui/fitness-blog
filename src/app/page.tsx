aee61e001dcf70116f888b4e55c5d7517242f85e
arshadsiddiqui <ferozarshad123@gmail.com>
2025-04-16 18:49:30 +0100

    Store a filter in the URl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,10 @@ export default function Home() {
         {routes.map((route) => (
           <div key={route.name} className="max-w-md w-full m-2">
             <Link
-              href={"/" + route.name.toLowerCase()}
+              href={{
+                pathname: '/posts',
+                query: { filter: route.name.toLowerCase()}
+              }}
               className={`${route.bgColor} block text-center p-2`}
             >
               {route.name}
